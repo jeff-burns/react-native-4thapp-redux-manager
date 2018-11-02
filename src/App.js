910efbe1e93ee32cf152f00107af4cab4371fe82
jeff-burns <1jeffreyburns@gmail.com>
2018-11-02 15:30:41 -0600

    added login, create user and authentication failed functionalities to the loginform

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyB0-pMVLNB1h36QDd00jmpmT4bjT0-2IUw',
+      authDomain: 'manager-rn-redux.firebaseapp.com',
+      databaseURL: 'https://manager-rn-redux.firebaseio.com',
+      projectId: 'manager-rn-redux',
+      storageBucket: 'manager-rn-redux.appspot.com',
+      messagingSenderId: '501128876113'
+    };
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/common/index.js
@@ -0,0 +1,7 @@
+// doing this way will both import and export the whole component in one line
+export * from './Button';
+export * from './Card';
+export * from './CardSection';
+export * from './Header';
+export * from './Input';
+export * from './Spinner';
