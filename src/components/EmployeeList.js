0f031885b4b67a54d7c11ea3db677cc51c642b0b
jeff-burns <1jeffreyburns@gmail.com>
2018-11-03 14:39:05 -0600

    employee list renders and you can add a new employee that is immediately rendered to the list

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,41 +1,27 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { ListView, View, Text } from 'react-native';
+import { FlatList } from 'react-native';
 import { employeesFetch } from '../actions';
+import ListItem from './ListItem';
 
 class EmployeeList extends Component {
     componentWillMount() {
         this.props.employeesFetch();
-
-        this.createDataSource(this.props);
     }
 
-    componentsWillReceiveProps(nextProps) {
-        // nextProps are the next set of props that the component will be rendered with
-        //this.props is still the old set of props
-        this.createDataSource(nextProps);
-    }
-
-    createDataSource({ employees }) {
-        const ds = new ListView.DataSource({
-            rowHasChanged: (r1, r2) => r1 !== r2
-        });
-
-        this.dataSource = ds.cloneWithRows(employees);
+    renderItem({ item }) {
+        console.log(this.item)
+        return <ListItem employee={item} />
     }
 
     render() {
         console.log(this.props)
         return (
-            <View>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-                <Text>Employee List</Text>
-            </View>
+            <FlatList
+                data={this.props.employees}
+                renderItem={this.renderItem}
+            />
         );
     }
 }
