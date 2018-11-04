27eb11ea154af20e729a8a0bdf01910f523f9c4c
jeff-burns <1jeffreyburns@gmail.com>
2018-11-03 20:10:52 -0600

    everything works in simulator

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeeList';
 import EmployeeCreate from './components/EmployeeCreate';
-
+import EmployeeEdit from './components/EmployeeEdit';
 
 const RouterComponent = () => {
     return (
@@ -21,6 +21,7 @@ const RouterComponent = () => {
                         title="Employees" 
                     />
                     <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+                    <Scene key="employeeEdit" component={EmployeeEdit} title="Edit Employee" />
                 </Scene>
             </Scene>
         </Router>
--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,6 +1,9 @@
 import {
     EMPLOYEE_UPDATE,
-    EMPLOYEE_CREATE
+    EMPLOYEE_CREATE,
+    EMPLOYEE_SAVE_SUCCESS,
+    EMPLOYEE_DELETE_SUCCESS,
+    EMPLOYEE_CLEAR_SUCCESS
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -15,6 +18,12 @@ export default (state = INITIAL_STATE, action) => {
             // action.payload === { prop: 'name', value: 'jane' }
             return { ...state, [action.payload.prop]: action.payload.value };
         case EMPLOYEE_CREATE:
+            return INITIAL_STATE;
+        case EMPLOYEE_SAVE_SUCCESS:
+            return INITIAL_STATE;
+        case EMPLOYEE_DELETE_SUCCESS:
+            return INITIAL_STATE;
+        case EMPLOYEE_CLEAR_SUCCESS:
             return INITIAL_STATE;
         default:
             return state;
